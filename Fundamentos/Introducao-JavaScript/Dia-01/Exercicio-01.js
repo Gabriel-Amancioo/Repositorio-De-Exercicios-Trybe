f8407ceaa5549cee1a49184ff0e36661cd65b979
Gabriel Amâncio <gabrieljesuino1133@outlook.com>
2023-05-01 16:43:08 -0300

    primeiro exerccício JavaScript

---
+++ Fundamentos/Introducao-JavaScript/Dia-01/Exercicio-01.js
@@ -0,0 +1,6 @@
+let myName = 'Gabriel';
+let birthCity = 'Guarujá';
+let birthYear = 1996;
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
---
+++ Fundamentos/Introducao-JavaScript/Dia-01/README.md
@@ -0,0 +1,1 @@
+# Exercícios de introdução ao JavaScript
